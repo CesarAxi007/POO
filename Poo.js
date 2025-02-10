660ab32fd3d562d98f85be39a235280462ade44b
CesarAxi007 <cesar.gomeze@axity.com>
2025-02-10 10:03:58 -0600

    first commit

---
+++ Poo.js
@@ -0,0 +1,23 @@
+var Gatos = /** @class */ (function () {
+    function Gatos(name, sex, age, weight, color, texture) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+        this.texture = texture;
+    }
+    Gatos.prototype.mostrarDatos = function () {
+        console.log("Name: ".concat(this.name));
+        console.log("Sex: ".concat(this.sex));
+        console.log("Age: ".concat(this.age));
+        console.log("Weight: ".concat(this.weight));
+        console.log("Color: ".concat(this.color));
+        console.log("Texture: ".concat(this.texture));
+    };
+    return Gatos;
+}());
+var gatoUno = new Gatos('Oscar', "Macho", 3, 7, 'Marron', "Rayado");
+var gatoDos = new Gatos('Luna', "Hembra", 2, 5, 'Gris', "Lisa");
+gatoUno.mostrarDatos();
+gatoDos.mostrarDatos();
---
+++ Poo.ts
@@ -0,0 +1,34 @@
+
+class Gatos {
+    name: string
+    sex: string
+    age: number
+    weight: number
+    color: string
+    texture: string
+    
+    constructor(name: string, sex: string, age: number, weight: number, color: string, texture: string) {
+        this.name = name
+        this.sex = sex
+        this.age = age
+        this.weight = weight
+        this.color = color
+        this.texture = texture
+    }
+
+    mostrarDatos(): void {
+        console.log(`Name: ${this.name}`)
+        console.log(`Sex: ${this.sex}`)
+        console.log(`Age: ${this.age}`)
+        console.log(`Weight: ${this.weight}`)
+        console.log(`Color: ${this.color}`)
+        console.log(`Texture: ${this.texture}`)
+    }
+}
+
+
+const gatoUno = new Gatos('Oscar', `Macho`, 3, 7, 'Marron', `Rayado`)
+const gatoDos = new Gatos('Luna', `Hembra`, 2, 5, 'Gris', `Lisa`)
+
+gatoUno.mostrarDatos()
+gatoDos.mostrarDatos()
